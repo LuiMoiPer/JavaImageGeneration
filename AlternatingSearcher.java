511ffe0b37fd29d0606f8a707c5a36ced24a8649
LuiMoiPer <luimoiper@outlook.com>
2020-11-14 22:38:25 -0800

    Added DepthFirstSearcher and AlternatingBfsDfsSearcher

---
+++ AlternatingSearcher.java
@@ -0,0 +1,84 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class AlternatingSearcher extends Searcher {
+
+    private int bfsSteps;
+    private int dfsSteps;
+    private int stepsTillSwitch;
+    private boolean bfs;
+
+    public AlternatingSearcher(
+        BufferedImage image,
+        Set<Point> visited,
+        UniqueDeque<Point> frontier,
+        PriorityQueue<Color> colorProvider,
+        Point[] neighbors,
+        boolean shuffleNeighbors,
+        int bfsSteps,
+        int dfsSteps
+    ) {
+        super(image, visited, frontier, colorProvider, neighbors, shuffleNeighbors);
+        this.bfsSteps = bfsSteps;
+        this.dfsSteps = dfsSteps;
+        this.bfs = true;
+        this.stepsTillSwitch = bfsSteps;
+    }
+
+    public AlternatingSearcher(
+        BufferedImage image,
+        Set<Point> visited,
+        UniqueDeque<Point> frontier,
+        PriorityQueue<Color> colorProvider,
+        Point[] neighbors,
+        boolean shuffleNeighbors,
+        int steps
+    ) {
+        this(image, visited, frontier, colorProvider, neighbors, shuffleNeighbors, steps, steps);
+    }
+
+    @Override
+    public boolean isDone() {
+        if (frontier.isEmpty() || colorProvider.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void seed(Point[] points) {
+        for (Point point : points) {
+            frontier.addFirst(point);
+        }
+    }
+
+    @Override
+    protected Point nextPoint() {
+        // do the step
+        Point point;
+        if (bfs) {
+            point = frontier.pollFirst();
+        }
+        else {
+            point = frontier.pollLast();
+        }
+        // update control mechanism
+        if (--stepsTillSwitch == 0) {
+            if (bfs == true) {
+                stepsTillSwitch = dfsSteps;
+            }
+            else {
+                stepsTillSwitch = bfsSteps;
+            }
+            bfs = !bfs;
+        }
+        return point;
+    }
+
+    @Override
+    protected Color nextColor() {
+        return colorProvider.poll();
+    }   
+}
---
+++ DfSearcher.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class DfSearcher extends Searcher {
+    public DfSearcher(
+        BufferedImage image,
+        Set<Point> visited,
+        UniqueDeque<Point> frontier,
+        PriorityQueue<Color> colorProvider,
+        Point[] neighbors,
+        boolean shuffleNeighbors
+    ) {
+        super(image, visited, frontier, colorProvider, neighbors, shuffleNeighbors);
+    }
+
+    @Override
+    public boolean isDone() {
+        if (frontier.isEmpty() || colorProvider.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void seed(Point[] points) {
+        // maybe do something to make all the points added get visited next?
+        for (Point point : points) {
+            frontier.addLast(point);
+        }
+    }
+
+    @Override
+    protected Point nextPoint() {
+        return frontier.pollLast();
+    }
+
+    @Override
+    protected Color nextColor() {
+        return colorProvider.poll();
+    }
+}
