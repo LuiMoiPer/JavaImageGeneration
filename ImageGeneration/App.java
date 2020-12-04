de8b1f239eab2e989766c879cf4402c56bc623c3
LuiMoiPer <luimoiper@outlook.com>
2020-12-03 22:11:05 -0800

    Got basic status posting, still working on image posting

--- ImageGeneration/App.java
+++ ImageGeneration/App.java
@@ -3,27 +3,52 @@ package ImageGeneration;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
 import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class App {
     private static String inputPath = "./Inputs/4.jpg";
     private static String outputPath = "./Outputs/";
+    private static String consumerKeyPath = "./Candy/ApiKey";
+    private static String consumerKeySecretPath = "./Candy/ApiSecretKey";
+    private static String accessTokenPath = "./Candy/AccessToken";
+    private static String accessTokenSecretPath = "./Candy/AccessTokenSecret";
     private static BufferedImage image;
+    private static Twitter twitter;
 
     public static void main(String[] args) {
         // get a tweet with an image from twitter
         // download the image
         // run image generation on the image
         // upload the image generated
+        /*
         loadImage(inputPath);
         bfs(image);
         saveImage(image, outputPath, 0);
+        */
+        twitter = getTwitterInstance();
+        try {
+            File imageFile = new File("./Outputs/2020.11.21.01.22.42.765.png");
+            twitter.uploadMedia(imageFile);
+            Status status = twitter.updateStatus("Testing image upload");
+        }
+        catch (TwitterException exception) {
+            System.out.println(exception.getMessage());
+        }
+        catch (Exception Exception) {
+            System.out.println(Exception.getMessage());
+        }
     }
 
     static void bfs(BufferedImage image) {
@@ -93,7 +118,30 @@ public class App {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    static void postNewImageToTwitter() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    static String getFileContents(String path) {
+        String contents = "";
+        try {
+            Scanner fileReader = new Scanner(new File(path));
+            contents = fileReader.nextLine();
+        }
+        catch (FileNotFoundException exception) {
+            System.out.println("File not found: " + path);
+        }
+        return contents;
+    }
+
+    static Twitter getTwitterInstance() {
+        String consumerKey = getFileContents(consumerKeyPath);
+        String consumerSecret = getFileContents(consumerKeySecretPath);
+        String accessToken = getFileContents(accessTokenPath);
+        String accessTokenSecret = getFileContents(accessTokenSecretPath);
+
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setOAuthConsumerKey(consumerKey)
+            .setOAuthConsumerSecret(consumerSecret)
+            .setOAuthAccessToken(accessToken)
+            .setOAuthAccessTokenSecret(accessTokenSecret);
+        TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
+        return twitterFactory.getInstance();
     }
 }
