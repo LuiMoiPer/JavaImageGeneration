44cc17955147e71c475191664e76d248e49128ae
LuiMoiPer <luimoiper@outlook.com>
2020-12-11 18:38:33 -0800

    Worked on image resizing and gettingStausWithImage. Seems like I can't directly search for tweets with images so looking at using twitter API v2

--- ImageGeneration/TwitterDriver.java
+++ ImageGeneration/TwitterDriver.java
@@ -9,11 +9,15 @@ import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
+import twitter4j.HttpParameter;
+import twitter4j.MediaEntity;
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterDriver {
@@ -21,14 +25,50 @@ public class TwitterDriver {
     private static String consumerKeySecretPath = "./Candy/ApiSecretKey";
     private static String accessTokenPath = "./Candy/AccessToken";
     private static String accessTokenSecretPath = "./Candy/AccessTokenSecret";
-    private static String tempPath = "./temp";
-    private static int MAX_UPLOAD_SIZE;
     private static Twitter twitter;
+
+    private final static String TEMP_PATH = "./temp";
+    private final static String RESIZE_PATH = "./tempResize";
+    private static File tempImg = new File(TEMP_PATH + ".png");
+    private static File tempResizedImg = new File(RESIZE_PATH + ".png");
+    private static long MAX_UPLOAD_SIZE = 5242880;
+
     static {
         setupTwitterInstance();
+        try {
+            tempImg.createNewFile();
+            tempResizedImg.createNewFile();
+        }
+        catch (Exception exception) {
+            System.out.println("Exception happened in creating file");
+        }
     }
 
     private TwitterDriver() {}
+
+    private static void cleanupTempFiles() {
+        tempImg.delete();
+        tempResizedImg.delete();
+    }
+
+    private static String getAuthFileContents(String path) {
+        String contents = "";
+        try {
+            Scanner fileReader = new Scanner(new File(path));
+            contents = fileReader.nextLine();
+        }
+        catch (FileNotFoundException exception) {
+            System.out.println("File not found: " + path);
+        }
+        return contents;
+    }
+
+    private static File resizeImage(BufferedImage image) {
+        // while resized image size is larger than max upload size
+        //   attempt resize
+        // return resized file
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 
     private static void setupTwitterInstance() {
         String consumerKey = getAuthFileContents(consumerKeyPath);
@@ -42,18 +82,6 @@ public class TwitterDriver {
             .setOAuthAccessTokenSecret(accessTokenSecret);
         TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
         twitter = twitterFactory.getInstance();
-    }
-
-    private static String getAuthFileContents(String path) {
-        String contents = "";
-        try {
-            Scanner fileReader = new Scanner(new File(path));
-            contents = fileReader.nextLine();
-        }
-        catch (FileNotFoundException exception) {
-            System.out.println("File not found: " + path);
-        }
-        return contents;
     }
 
     private static StatusUpdate setupStatusUpdate(BufferedImage image, Status originalStatus) {
@@ -75,33 +103,53 @@ public class TwitterDriver {
     public static File getUploadableImage(BufferedImage image) {
         // save image as both png and jpg
         File output = null;
-        File png = new File(tempPath + ".png");
-        File jpg = new File(tempPath + ".jpg");
         try {
-            ImageIO.write(image, "png", png);
-            ImageIO.write(image, "jpg", jpg);
+            ImageIO.write(image, "png", tempImg);
+            System.out.println("PNG size: " + Files.size(tempImg.toPath()));
             // if the png is less than max upload return that
-            if (Files.size(png.toPath()) > MAX_UPLOAD_SIZE) {
-                output = png;
-            }
-            // else if jpg is less than max upload return that
-            else if (Files.size(jpg.toPath()) > MAX_UPLOAD_SIZE) {
-                output = jpg;
+            if (Files.size(tempImg.toPath()) < MAX_UPLOAD_SIZE) {
+                System.out.println("Setting output to png");
+                output = tempImg;
             }
             // else resize
             else {
-                throw new UnsupportedOperationException("Resize not yet implemented");
+                System.out.println("Resizing image ");
+                output = resizeImage(image);
             }
         } 
         catch (IOException exception) {
             System.out.println("=== IO Exception ===");
             System.out.println(exception.getMessage());
+            exception.printStackTrace();
         }
         return output;
     }
 
     public static Status getStatusWithImage() {
         // search twitter for a status that also has an image
+        Query query = new Query("art");
+        query.setCount(20);
+        query.setResultType(Query.POPULAR);
+        System.out.println(query.getQuery());
+        QueryResult result = null;
+        try {
+            result = twitter.search(query);
+        }
+        catch (TwitterException exception) {
+            System.out.println("=== Twitter Exception ===");
+            System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
+        for (Status status : result.getTweets()) {
+            System.out.println(status.getText());
+            System.out.println(status.getId());
+            MediaEntity[] mediaEntities = status.getMediaEntities();
+            for (MediaEntity mediaEntity : mediaEntities) {
+                System.out.println("Media of type: " + mediaEntity.getType());
+            }
+            //System.out.print("+");
+        }
+
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
