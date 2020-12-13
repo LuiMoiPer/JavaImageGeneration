0eed6e0b318183d304ddc0fc0aa0683e4401d711
LuiMoiPer <luimoiper@outlook.com>
2020-12-12 18:25:35 -0800

    First attempt at ImageAndStatus

--- ImageGeneration/ImageAndStatus.java
+++ ImageGeneration/ImageAndStatus.java
@@ -1,22 +1,49 @@
 package ImageGeneration;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 import twitter4j.Status;
 
 public class ImageAndStatus {
-
+    private static final String TEMP_PATH = "./Temp/";
+    private static final String IMAGE_NAME = "trendingImage";
     private final BufferedImage image;
     private final Status status;
 
+    private static BufferedImage downloadFromUrl(URL url) {
+        String fileExtension;
+        String urlString = url.toString();
+        int i = urlString.lastIndexOf('.');
+        fileExtension = urlString.substring(i + 1);
+        BufferedImage image = null;
+        
+        try {
+            String path = TEMP_PATH + IMAGE_NAME + "." + fileExtension; 
+            ImageIO.write(
+                ImageIO.read(url), 
+                fileExtension, 
+                new File(path)
+            );
+            image = ImageIO.read(new File(path));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        return image;
+    }
+
     public ImageAndStatus(URL url, Status status) {
-        // download the image from the url
-        throw new UnsupportedOperationException("Not implemented yet");
+        this(downloadFromUrl(url), status);
     }
 
     public ImageAndStatus(BufferedImage image, Status status) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        this.image = image;
+        this.status = status;
     }
 
     public Status getStatus() {
