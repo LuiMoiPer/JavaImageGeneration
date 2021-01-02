0d8e3106d845f93e4cbc12072c67cb768c48f024
LuiMoiPer <luimoiper@outlook.com>
2021-01-01 22:47:24 -0800

    Moved same image to Utils

--- ImageGeneration/Utils.java
+++ ImageGeneration/Utils.java
@@ -1,14 +1,20 @@
 package ImageGeneration;
 
 import java.awt.Color;
+
 import java.awt.Font;
+import java.awt.image.BufferedImage;
 import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
 import java.lang.Math;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
+import javax.imageio.ImageIO;
 
 public class Utils {
     public static <T> void shuffleArray(T[] array) {
@@ -62,5 +68,14 @@ public class Utils {
         Font[] fonts = env.getAllFonts();
         Random random = ThreadLocalRandom.current();
         return fonts[random.nextInt(fonts.length)];
+    }
+
+    public static void saveImage(BufferedImage image, String path, int millsToSleep) {
+        try {
+            ImageIO.write(image, "png", new File(path + Utils.timestamp() + ".png"));
+        } catch (IOException exception) {
+            System.out.println("Could not read file: " + path);
+            System.exit(2);
+        }
     }
 }
