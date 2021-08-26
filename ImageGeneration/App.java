01a4df6976e7eaa1c09307d9763014a6ac4a0b8d
LuiMoiPer <luimoiper@outlook.com>
2021-08-26 16:47:24 -0700

    Updated App imports

--- ImageGeneration/App.java
+++ ImageGeneration/App.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+import ImageGeneration.Neighbors.NeighborFactory;
 import ImageGeneration.Searchers.Searcher;
 import ImageGeneration.Searchers.SearcherFactory;
 
